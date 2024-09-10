ad68bfff8b5a1d5ac9799fd887245539bc03ff5a
Puppycoder88 <jaingarima1795@gmail.com>
2024-09-10 13:10:11 +0530

    bb

--- src/components/About/BrochureCard.jsx
+++ src/components/About/BrochureCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { FaFilePdf } from 'react-icons/fa';
 
@@ -7,8 +8,8 @@ const BrochureCard = () => {
     <div className="bg-green-500 p-8 rounded-3xl text-white w-64 font-custom">
       <FaFilePdf size={40} className="mb-4" />
       <p className="text-md mb-2">Just starting out?</p>
-      <h2 className="text-xl font-bold mb-4 ">Download a company brochure</h2>
-      <a href='#' className="bg-white text-blue-500 hover:bg-red-500 hover:text-white font-semibold py-2 px-2 rounded-3xl flex items-center justify-center">
+      <h2 className="text-xl font-bold mb-4">Download a company brochure</h2>
+      <a href='/src/assets/Brochure.pdf' download='Brochure.pdf' className="bg-white text-blue-500 hover:bg-red-500 hover:text-white font-semibold py-2 px-2 rounded-3xl flex items-center justify-center">
         Get A Copy <span className="ml-2">→</span>
       </a>
     </div>
